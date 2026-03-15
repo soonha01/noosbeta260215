4b60d4a9aa98f45a1a2a78d604f3855db5b73c78
kwonsoonha <jh1man@naver.com>
2026-03-15 16:14:47 +0900

    깃 provider_id -> Null값들어옴 Google은 sub GitHub는 id

--- backend/src/main/java/com/noos/backend/auth/service/CustomOAuth2UserService.java
+++ backend/src/main/java/com/noos/backend/auth/service/CustomOAuth2UserService.java
@@ -22,26 +22,40 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        //기본 사용자 정보를 Map으로 가져옴
         OAuth2User oAuth2User = super.loadUser(userRequest);
-
-        // 구글,github가 보낸 속성들 추출
         Map<String, Object> attributes = oAuth2User.getAttributes();
-        String registrationId = userRequest.getClientRegistration().getRegistrationId(); // "google or GitHub"
-        String providerId = String.valueOf(attributes.get("sub")); // 고유 식별값
-        String email = (String) attributes.get("email");
-        String name = (String) attributes.get("name");
 
-        System.out.println("OAuth 로그인 정보 저장 시작: " + email);
+        // "google" 또는 "github"
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
 
-        //DTO 객체 생성 및 데이터 세팅
+        String providerId = "";
+        String email = "";
+        String name = "";
+
+        //플랫폼별로 데이터를 다르게 뽑아냄
+        if ("google".equals(registrationId)) {
+            providerId = String.valueOf(attributes.get("sub"));
+            email = (String) attributes.get("email");
+            name = (String) attributes.get("name");
+        } else if ("github".equals(registrationId)) {
+            providerId = String.valueOf(attributes.get("id"));
+            email = (String) attributes.get("email");
+            name = (String) attributes.get("name");
+
+            // 깃허브는 이름을 안 적은 유저시 -> Login id로 그냥 넣어버림
+            if (name == null) {
+                name = (String) attributes.get("login");
+            }
+        }
+
+        System.out.println(registrationId + " 로그인 정보 저장 시작: " + email);
+
         User user = new User();
-        user.setLoginId(email);      // login_id 컬럼에 이메일 저장
-        user.setDisplayName(name);   // display_name 컬럼에 이름 저장
-        user.setProvider(registrationId); // google or GitHub 저장
-        user.setProviderId(providerId);   // 고유 식별값 저장
+        user.setLoginId(email);
+        user.setDisplayName(name);
+        user.setProvider(registrationId);
+        user.setProviderId(providerId);
 
-        //DB 저장 로직 호출
         authMapper.saveOrUpdateOAuthUser(user);
 
         return oAuth2User;
