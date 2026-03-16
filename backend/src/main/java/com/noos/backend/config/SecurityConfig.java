15583d88d1134e2f1b29ff7fa537605bea505afc
kwonsoonha <jh1man@naver.com>
2026-03-16 13:21:31 +0900

    소셜로그인 -> 기기화면으로

--- backend/src/main/java/com/noos/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/noos/backend/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.noos.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,13 +20,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable()) // 테스트를 위해 CSRF 비활성화
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/login/**").permitAll() // 메인과 로그인 페이지는 누구나 접근 가능
+                        .requestMatchers("/", "/login/**", "/api/auth/**").permitAll() // 메인과 로그인 페이지는 누구나 접근 가능
                         .anyRequest().authenticated() // 나머지는 인증 필요
                 )
                 .oauth2Login(oauth2 -> oauth2
-                        .defaultSuccessUrl("http://localhost:3000/main", true) // 로그인 성공 시 프론트엔드로 리다이렉트
+                        .defaultSuccessUrl("http://localhost:3000/?login=success", true) // 로그인 성공 시 프론트엔드로 리다이렉트
                 );
 
         return http.build();
